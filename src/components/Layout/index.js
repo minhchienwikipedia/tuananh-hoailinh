8fb44180d1a6fdd18372fb091cec2ed61efc46cb
Duong Minh Chien <minhchienwikipedia@gmail.com>
2023-09-09 23:59:46 +0700

    Update

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,13 +2,13 @@ import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import { node } from 'prop-types';
 
-import Favicon from '@assets/images/dinda-indra.png';
+import Favicon from '@assets/images/logo.png';
 import '@assets/css/icomoon.css';
 import '@assets/css/bootstrap.css';
 import '@assets/css/style.css';
 
 const IMAGE_URL = `https://github.com/minhchienwikipedia/tuananh-hoailinh/blob/gh-pages/static/bg-welcome-809077a12bd83875c2c21399840b5880.jpg?raw=true`;
-const META_DESCRIPTION = `Dengan memohon Rahmat dan Ridho Illahi, teriring niat menjalankan Sunnah Rasulullah ﷺ untuk membentuk rumah tangga yang Sakinah, Mawaddah wa Rahmah, kami mohon do'a agar senantiasa diberikan kelancaran dan keberkahan. - Tuấn Anh & Hoài Linh`;
+const META_DESCRIPTION = `Tuấn Anh ❤️ Hoài Linh Wedding`;
 
 function MainLayout({ children }) {
   return (
@@ -22,7 +22,6 @@ function MainLayout({ children }) {
         {/* font and SEO tags */}
         <meta property="og:title" content="The Wedding of Tuấn Anh & Hoài Linh" />
         <meta property="og:image" content={IMAGE_URL} />
-        <meta property="og:url" content="https://thekusuma.com" />
         <meta property="og:site_name" content="The Wedding of Tuấn Anh & Hoài Linh" />
         <meta property="og:description" content={META_DESCRIPTION} />
 
